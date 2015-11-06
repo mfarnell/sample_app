a051cfcc35dfd5261ec6acfc7303b33b1e43689d
Marvin Kendrick <mkendrick@greenriver.org>
2015-11-06 08:32:42 -0500

    initial chapter 4 work

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,15 +1,22 @@
 class StaticPagesController < ApplicationController
   
   def home
+    @title = "Home"
   end
 
   def help
+    @title = "Help"
   end
 
   def about
+    @title = "About"
   end
 
   def contact
+    @title = "Contact"
+  end
+
+  def marvin_
   end
 
 end
