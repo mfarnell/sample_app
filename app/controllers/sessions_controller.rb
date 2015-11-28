9121df377e3c112eb7e4977bed25ff224de69469
Marvin Kendrick <mkendrick@greenriver.org>
2015-11-28 14:09:00 -0500

    initial login logout framework

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      flash[:success] = "Welcome back!"
+      log_in user
+      redirect_to user
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
+
+  def destroy
+    log_out
+    flash[:success] = "You are now logged out!"
+    redirect_to root_url
+  end
+
+end
